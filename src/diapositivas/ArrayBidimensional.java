47e9525f419f834b57935f1a119d00a8859369f8
Luis Usero Reyes <l.usero.dam@gmail.com>
2015-01-16 11:12:34 +0100

    modificamos el archivo array para cambiar la matriz

--- src/diapositivas/ArrayBidimensional.java
+++ src/diapositivas/ArrayBidimensional.java
@@ -3,23 +3,28 @@ package diapositivas;
 public class ArrayBidimensional {
 
 	public static void main(String[] args) {
-		double [][] TABLA = {{7,5,3},{3,4,7},{8,4,8},{3,4,8}};
+		double [][] TABLA = {{0,1,2},
+							{3,4,5},
+							{6,7,8},
+							{9,10,11}};						
 		double suma=0;
 		double contador=0;
 		for (int i = 0; i < TABLA.length; i++) {
-			System.out.println("");
-		for (int j = 0; j < TABLA[0].length; j++) {
-			System.out.print(TABLA[i][j]+"\t");
+			
+		for (int j = 0; j < TABLA[i].length; j++) {
+			System.out.print("Posicion "+i+","+j+" : " + TABLA[i][j]+"\t");
 			suma+= TABLA[i][j];
 			contador++;
 		}
-			
+		System.out.println("");
 	}
 		double media=suma/contador;
 		System.out.printf("\nMedia de los valores es = %.2f",media);
-		
-		
-
+		//definimos una nueva matriz y los valores estan invertido
+		//es decir el primero de este nuevo array es el ultimo
+		//del array anterior
+		double [][] NTABLA = new double [4][3];
+			
 	}
 
 }
