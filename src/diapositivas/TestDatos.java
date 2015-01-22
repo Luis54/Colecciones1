8911913f3fcbe12e8aa09f3b251e753267063dc1
Luis Usero Reyes <luis@luis-VPCEH2J1E>
2015-01-22 22:28:40 +0100

    modifica el ejercicio 3 intentando crear un arraylist

--- src/diapositivas/TestDatos.java
+++ src/diapositivas/TestDatos.java
@@ -1,10 +1,17 @@
 package diapositivas;
 
+import java.util.ArrayList;
+
 public class TestDatos {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ArrayList <Double> t = new ArrayList <Double>();
+		System.out.println("Tamaño de la lista es : "+t.size());
+		//t.add(0, 5,25);
+		System.out.println(t);
+	
+		
 		
 		
 		
